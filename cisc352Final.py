6e939d3b27c0d5b23d300e8d002dce14449bfe69
Brandon Christof <brandonchristof@gmail.com>
2019-02-15 09:18:15 -0500

    Finalized code with comments and optimization

--- cisc352Final.py
+++ cisc352Final.py
@@ -1,71 +1,82 @@
-"""
-CISC352 Assignment 1
-Group 2
-Febuary 15th 2019
-Authors:
-Michael Olson    20008033
-Brandon Christof 20014247
-"""
+'''
+    CISC 352 Assignment 1
+    Date Created:  2019-02-07
+    Last Modified: 2019-02-15
+    Group 2
+    Authors:
+        Brandon Christof 20014247
+        Michael Olson    20008033
+'''
 import random
 import math
 import time
 from math import trunc
+
+## Changes conflicts for each list
+def changeState(csp, x, y, v):
+    csp.rowConflicts[x] += v
+    csp.leftDiag[csp.n + x - y - 1] += v
+    csp.rightDiag[x + y] += v
   
-## This is the chess board. The chess board should manage
-##      the queens, and keep track of positons and conflicts
+## Manages the queens positions and conflicts
 class Board():
     def __init__(self, n):
+
+        # Size of chessboard
         self.n = n
-        ## 2D matrix of 0 if no queen and instance in queen's position
-        ## List of queen instances.
+        
+        # List of queen positions
         self.queens = []
         
-        ## List of queens that currently have at least one conflict
-        self.conflicts = []
+        # Conflicts for all right diagonals, left diagonals and rows
         self.rowConflicts = [0] * n
         self.leftDiag = [0] * ((2 * n) - 1)
         self.rightDiag = [0] * ((2 * n) - 1)
 
-    ## If self.queens is empty, create queens
-    ## randomize the postions, with one queen per row
-    
+
+    ## Places queens on chessboard at best position using Greedy Search
     def setQueens(self, differentOrder):
 
+        # Creates a list of [0, 1, 2... n]
         col = list(range(self.n))
+
+        # Keeps track of queen positions not set
         notSet = []
 
         # If last order didn't work within step count, try this order
         if differentOrder:
             f = col.pop(0)
             col.append(f)
-        
+
+        # Loop to place queens, all at positions with 0 conflicts
         for i in range(self.n):
             ql = len(self.queens)
             test = col.pop(0)
             columnConflict = self.rowConflicts[test] + self.rightDiag[ql + test] + self.leftDiag[self.n + test - ql - 1]
             if columnConflict == 0:
                 self.queens.append(test)
-                self.rowConflicts[test] += 1
-                self.leftDiag[self.n + test - ql - 1] += 1
-                self.rightDiag[test + ql] += 1
+                changeState(self, test, ql, 1)
             else:
                 notSet.append(test)
                 
         ns = len(notSet)
-        
+        idx = len(self.queens)
+
+        # Places queens at the rest of the positions if needed
         if ns > 0:
             idx = len(self.queens)
             for k in range(ns):
                 self.queens.append(notSet[k])
-                self.rowConflicts[notSet[k]] += 1
-                self.leftDiag[self.n + notSet[k] - idx - 1] += 1
-                self.rightDiag[notSet[k] + idx] += 1
+                changeState(self, notSet[k], idx, 1)
                 idx+=1
 
-
-    def getConflicts(self):
+    ## Returns the row with the most conflicts. If there are no conflicts, returns -1.
+    ##      If there is a tie, randomly chooses
+    def getMostConflicted(self):
+        
         most = 1
         positions = []
+        
         for i in range(self.n):
             c = self.rowConflicts[self.queens[i]] + self.rightDiag[i + self.queens[i]] + self.leftDiag[self.n + self.queens[i] - i - 1] - 3
             if c > most:
@@ -78,33 +89,22 @@ class Board():
             return -1
         return random.choice(positions)
 
-## Main min conflicts algoritm, see assignment for algorithm.
-##      return solution, or None if no solution is found.
+## Finds queen with most conflicts, and places it in best position
+##      until solved or until it reaches too many attempts
 def minConflicts(csp, maxSteps):
     for i in range(maxSteps):
-        m = csp.getConflicts()
+        m = csp.getMostConflicted()
         if (m == -1):
             return csp.queens
-
         findLeastconflicts(csp, m)
-    return -1   
+    return -1
 
-## Should convert an instance of Board to a list of queen positions
-## ex. return [2,0,1,4]
-def convertBoard(board):
-    positions = []
-    for q in board.queens:
-        positions.append(i.y)
-    return positions
-    
+## Takes the queen with the most conflicts and places it at least
+##      conflicted position. If there is a tie, randomly chooses
 def findLeastconflicts(csp, idx):
 
     yPos = csp.queens[idx]
-    
-    csp.rowConflicts[yPos] -=1
-    csp.leftDiag[csp.n + yPos - idx - 1] -=1
-    csp.rightDiag[yPos + idx] -=1
-    
+    changeState(csp, yPos, idx, -1)
     numConflicts = csp.n
     bestPos = []
     
@@ -112,9 +112,7 @@ def findLeastconflicts(csp, idx):
         c = csp.rowConflicts[i] + csp.rightDiag[i + idx] + csp.leftDiag[csp.n + i - idx - 1]
         if c == 0:
             csp.queens[idx] = i
-            csp.rowConflicts[i] +=1
-            csp.leftDiag[csp.n + i - idx - 1] +=1
-            csp.rightDiag[i + idx] +=1
+            changeState(csp, i, idx, 1)
             return i
         if c < numConflicts:
             numConflicts = c
@@ -124,13 +122,11 @@ def findLeastconflicts(csp, idx):
 
     pos = random.choice(bestPos)
 
-    csp.queens[idx] = pos 
-    csp.rowConflicts[pos] +=1
-    csp.leftDiag[csp.n + pos - idx - 1] +=1
-    csp.rightDiag[pos + idx] +=1
+    csp.queens[idx] = pos
+    changeState(csp, pos, idx, 1)
 
 
-## Should read in "nqueens.txt" and return list of problems to solve
+## Reads in "nqueens.txt" and returns list of problems to solve
 def inputFile(fileName):
     with open(fileName) as f:
         content = f.readlines()
@@ -144,35 +140,42 @@ def outputFile(fileName, solutions):
     f.close()
         
 
+## Makes sure n value isn't too big.
+## Creates a new board and attempts to solve in less than 60 moves.
+## If algorithm is unable to, tries again with a different start order.
+## Returns queens list when solved
 def runAlgorithm(n):
+    
     if n > 2000000:
         print("Too large of n")
         exit(0)
+        
     csp = Board(n)
     csp.setQueens(False)
     trials = 60
+    
     while minConflicts(csp, trials) == -1:
         csp = Board(n)
         csp.setQueens(True)
         trials = n*2
     return csp.queens
 
-## problems is the list of n size solutions we must find
-problems = []
-## solutions is a list of lists, where each element is
-##      a matrix of positons as shown in assignment
-solutions = []
+problems = []       # List of n sized problems
+solutions = []      # Contains solutions for each problem
 
 problems = inputFile("nqueens.txt")
 input("Press Enter to start:")
-time0 = time.time()
+
+time0 = time.time() #Starts timer
+
+# Runs through each problem given
 for i in problems:
     solutions.append(runAlgorithm(i))
-    
+
+# Stops timer when solved and prints time
 time1 = time.time()
 tot_time = time1 - time0
 time_string = str(trunc(tot_time*100)/100)
-print("\n   Took " + time_string + " seconds\n")
-print("Done")
-outputFile("nqueens_output.txt", solutions)
 
+print("\n   Time: " + time_string + " seconds\n")
+outputFile("nqueens_output.txt", solutions)
