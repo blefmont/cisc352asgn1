bca4c7c90b89b50584c6f571e4f01be34669800e
Michael Olson <michael.olson@queensu.ca>
2019-03-12 18:47:46 -0400

    code outline for alphabeta

--- alphabeta.py
+++ alphabeta.py
@@ -1,1 +1,38 @@
 ## Alpha-Beta Pruning
+
+
+## documentation
+class Node():
+    def _init__(self):
+        pass
+    ## documentation
+    def is_root_node(self):
+        pass
+    ## documentation
+    def is_leaf(self):
+        pass
+    ## documentation
+    def is_min_node(self):
+        pass
+    ## documentation
+    def is_max_node(self):
+        pass
+    ## documentation
+    def get_children(self):
+        pass
+## documentation
+def cut_off_search_below(current_node):
+    pass
+
+## documentation
+def alpha_beta(current_node, alpha, beta):
+    pass
+## documentation
+def input():
+    pass
+## documentation
+def output():
+    pass
+## documentation
+def parse_n_init():
+    pass
