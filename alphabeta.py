0a11e953c39cf6f6284c0afcb6f07f20c92e6503
Brandon Christof <brandonchristof@gmail.com>
2019-03-14 17:27:48 -0400

    Looping children

--- alphabeta.py
+++ alphabeta.py
@@ -3,40 +3,23 @@
 
 ## documentation
 class Node():
-    def _init__(self, isMaxNode, isRootNode, isLeafNode, value = None):
-        self.isMax = isMaxNode
-        self.isMin = not isMaxNode
-        self.isLeaf = isLeafNode
-        self.isRoot = isRootNode
-        self.value = value
-        if (isLeafNode and value == None):
-            raise ValueError("If is a leaf node, value must be provided")
-        self.children = []
+    def _init__(self):
+        pass
     ## documentation
     def is_root_node(self):
-        return self.isRoot
+        pass
     ## documentation
     def is_leaf(self):
-        return self.isLeaf
+        pass
     ## documentation
     def is_min_node(self):
-        return self.isMin
+        pass
     ## documentation
     def is_max_node(self):
-        return self.isMax
-    ## documentation
-    def add_child(self, node):
-        if (type(node) != type(self)):
-            print("Child must be a node")
-        elif (self.isLeaf):
-            print("Leaves can not have children")
-        else:
-            self.children.append(node)
-            
+        pass
     ## documentation
     def get_children(self):
-        return children
-    
+        pass
 ## documentation
 def cut_off_search_below(current_node):
     pass
@@ -64,10 +47,8 @@ def alpha_beta(current_node, alpha, beta):
 
 
 ## documentation
-def input(filename):
-    with open(fileName) as f:
-        content = f.read()
-    return content
+def input():
+    pass
 ## documentation
 def output():
     pass
