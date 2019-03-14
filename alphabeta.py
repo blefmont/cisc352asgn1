154f37c3e092f6164f378a4ca894979a467c46b6
Michael Olson <michael.olson@queensu.ca>
2019-03-14 16:44:46 -0400

    node class first implementation

--- alphabeta.py
+++ alphabeta.py
@@ -3,23 +3,40 @@
 
 ## documentation
 class Node():
-    def _init__(self):
-        pass
+    def _init__(self, isMaxNode, isRootNode, isLeafNode, value = None):
+        self.isMax = isMaxNode
+        self.isMin = not isMaxNode
+        self.isLeaf = isLeafNode
+        self.isRoot = isRootNode
+        self.value = value
+        if (isLeafNode and value == None):
+            raise ValueError("If is a leaf node, value must be provided")
+        self.children = []
     ## documentation
     def is_root_node(self):
-        pass
+        return self.isRoot
     ## documentation
     def is_leaf(self):
-        pass
+        return self.isLeaf
     ## documentation
     def is_min_node(self):
-        pass
+        return self.isMin
     ## documentation
     def is_max_node(self):
-        pass
+        return self.isMax
+    ## documentation
+    def add_child(self, node):
+        if (type(node) != type(self)):
+            print("Child must be a node")
+        elif (self.isLeaf):
+            print("Leaves can not have children")
+        else:
+            self.children.append(node)
+            
     ## documentation
     def get_children(self):
-        pass
+        return children
+    
 ## documentation
 def cut_off_search_below(current_node):
     pass
@@ -28,8 +45,10 @@ def cut_off_search_below(current_node):
 def alpha_beta(current_node, alpha, beta):
     pass
 ## documentation
-def input():
-    pass
+def input(filename):
+    with open(fileName) as f:
+        content = f.read()
+    return content
 ## documentation
 def output():
     pass
